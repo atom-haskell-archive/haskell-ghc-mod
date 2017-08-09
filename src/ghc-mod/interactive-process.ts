68858f12c54022266c2a63b91fd1101d97ffe184
Nikolay Yakimov <root@livid.pp.ru>
2017-08-10 00:44:01 +0300

    Define Symbol.asyncIterator

--- src/ghc-mod/interactive-process.ts
+++ src/ghc-mod/interactive-process.ts
@@ -4,6 +4,8 @@ import { EOL } from 'os'
 import * as CP from 'child_process'
 import Queue = require('promise-queue')
 
+(Symbol as any).asyncIterator = Symbol.asyncIterator || Symbol.for('Symbol.asyncIterator')
+
 export interface GHCModCaps {
   version: number[],
   fileMap: boolean,
